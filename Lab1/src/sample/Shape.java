560f5be1360aa47b28520c1ac1b3b92ae08ee9f5
Vlad R <vladrrv@gmail.com>
2019-02-17 21:07:42 +0300

    lab 1 begin

---
+++ Lab1/src/sample/Shape.java
@@ -0,0 +1,14 @@
+package sample;
+
+public abstract class Shape {
+    private javafx.geometry.Point2D theCenter;
+
+    public void draw() {
+    }
+
+    public void move() {
+    }
+
+    public void location() {
+    }
+}
