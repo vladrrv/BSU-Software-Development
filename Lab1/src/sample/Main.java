4a1284ec26c6ce08efbf360fcf064857b5cb521a
Vlad R <vladrrv@gmail.com>
2019-03-12 11:31:03 +0300

    Add "save" & "about", move icons

--- Lab1/src/sample/Main.java
+++ Lab1/src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +20,7 @@ public class Main extends Application {
         primaryStage.show();
         primaryStage.setMinHeight(primaryStage.getHeight());
         primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("res/icon_app.png")));
     }
 
     public static void main(String[] args) {
